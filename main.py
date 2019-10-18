172ef25a171672a6413f0b1514899a87d90c9f8a
Lijia Li <lil@oxy.edu>
2019-10-18 15:09:45 -0400

    add main script for LDA

---
+++ main.py
@@ -0,0 +1,47 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+
+
+def display_topics(model, feature_names, no_top_words):
+    """display the topics of from the LDA model"""
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic %d:" % (topic_idx))
+        print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
+
+
+def get_LDA_topics(documents, no_features=1000, no_topics=10, no_top_words=10, display=0):
+    """ generate LDA model from document
+
+    Attributes:
+        documents (list(str)): documents to process list of strings
+        no_features (int):
+        no_topics (int):
+        no_top_words (int):
+        display (boolean):
+    Return:
+        LDA model
+    """
+    # vectorize the documents
+    tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+    tf = tf_vectorizer.fit_transform(documents)
+    tf_feature_names = tf_vectorizer.get_feature_names()
+
+    # Run LDA
+    lda = LatentDirichletAllocation(n_components=no_topics, max_iter=5, learning_method='online',
+                                    learning_offset=50.,random_state=0).fit(tf)
+    if display:
+        display_topics(lda, tf_feature_names, no_top_words)
+
+    return lda
+
+
+def main():
+    dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
+    documents = dataset.data
+    get_LDA_topics(documents, display=1)
+
+
+if __name__ == "__main__":
+    main()
+
