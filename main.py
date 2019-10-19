3ea7a4c359c804ef7fde8ad45fbde8286a30d45e
Di <di1113@me.com>
2019-10-19 11:11:07 -0400

    working on cos_rel lookup

--- main.py
+++ main.py
@@ -9,9 +9,32 @@ from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 
-def articles_in_descending_relevance(folder_path, relevance_results):
+
+# Util Functions 
+def get_document_names(folder_path):
     files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
-    # print(similarity_results)
+    return files
+
+
+def get_documents_content(file_list, folder_path):
+    """get a list of string of files content from the folder"""
+    articles = []
+    stemmer = PorterStemmer()
+    for fileName in file_list:
+        with open(folder_path+fileName, 'rb') as f:
+            fileContent = f.read()
+            fileContent = fileContent.decode('utf-8')
+            pattern = re.compile('([^\s\w]|_)+')
+            fileContent = pattern.sub('', fileContent)
+            fileContent = re.sub('\s',' ',fileContent)
+            fileContent = re.sub('\d+','',fileContent)
+            fileContent = ",".join([stemmer.stem(word) for word in fileContent.split(" ")])
+        articles.append(fileContent)
+    return articles
+
+
+def get_articles_with_descending_relevance(folder_path, relevance_results):
+    files = get_document_names(folder_path)
     ranking = sorted(range(len(relevance_results)),key=relevance_results.__getitem__, reverse = True)
     articles_with_relevance = []
     for i in ranking:
@@ -20,10 +43,19 @@ def articles_in_descending_relevance(folder_path, relevance_results):
         rel_with_filename.append(files[i])
         articles_with_relevance.append(rel_with_filename)
     return articles_with_relevance
-    # print(ranking)
-    # print(similarity_results[ranking[0]])
 
 
+def get_document_with_cos_rel(document_name, folder_path, queryvector):
+    file_list = [document_name]
+    document_content = get_documents_content(file_list, folder_path)
+
+    cos_sim = calculate_cosine_similarity(document_content, folder_path, query_file)
+    print
+
+
+
+
+# Non Util Functions
 def calculate_cosine_similarity(documents, folder_path, query_file, no_features=1000):
     #get base vector
 
@@ -43,7 +75,7 @@ def calculate_cosine_similarity(documents, folder_path, query_file, no_features=
             vocab.append(w)
             queryvector.append(wdict[w])
 
-        files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
+        files = get_document_names(folder_path)
         tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, vocabulary = vocab)
         tf = tf_vectorizer.fit_transform(documents)
         doc_term_matrix = tf.todense()
@@ -59,23 +91,6 @@ def calculate_cosine_similarity(documents, folder_path, query_file, no_features=
             similarity_results.append(sim_result[0][0])
 
         return similarity_results
-
-def get_document_from_folder(folder_path):
-    """get a list of string of files content from the folder"""
-    files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
-    articles = []
-    stemmer = PorterStemmer()
-    for fileName in files:
-        with open(folder_path+fileName, 'rb') as f:
-            fileContent = f.read()
-            fileContent = fileContent.decode('utf-8')
-            pattern = re.compile('([^\s\w]|_)+')
-            fileContent = pattern.sub('', fileContent)
-            fileContent = re.sub('\s',' ',fileContent)
-            fileContent = re.sub('\d+','',fileContent)
-            fileContent = ",".join([stemmer.stem(word) for word in fileContent.split(" ")])
-        articles.append(fileContent)
-    return articles
 
 
 def display_topics(model, feature_names, no_top_words):
@@ -151,54 +166,18 @@ def get_LDA_topics(documents, no_features=1000, no_topics=10, no_top_words=10, d
     return lda, tf_feature_names
 
 
-def calculate_cosine_similarity(documents, folder_path, query_file, no_features=1000):
-    #get base vector
-
-    with open(query_file) as file:
-        words = file.read().split()
-        #count frequency
-        wdict = {}
-        for w in words:
-            if w in wdict:
-                wdict[w] += 1
-            else:
-                wdict[w] = 1
-        #get vocab and freq array
-        vocab = []
-        queryvector = []
-        for w in wdict:
-            vocab.append(w)
-            queryvector.append(wdict[w])
-
-        files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
-        tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, vocabulary = vocab)
-        tf = tf_vectorizer.fit_transform(documents)
-        doc_term_matrix = tf.todense()
-        # show counterVectorizer result 
-        # df = pd.DataFrame(doc_term_matrix,
-        #               columns=tf_vectorizer.get_feature_names(),
-        #               index=files)
-        # print(df)
-        similarity_results = []
-        for vector in doc_term_matrix:
-            # print(cosine_similarity([queryvector], vector))
-            sim_result = cosine_similarity([queryvector], vector)
-            similarity_results.append(sim_result[0][0])
-
-        return similarity_results
-
-
 def main():
     n_top_words = 10
     folder_path = "./articles/"
     query_path = "./query.txt"
-    documents = get_document_from_folder(folder_path)
+    file_list = get_document_names(folder_path)
+    documents = get_documents_content(file_list, folder_path)
     lda, tf_feature_names = get_LDA_topics(documents, no_topics=100, no_top_words=n_top_words, display=0)
     build_query(lda, tf_feature_names, no_top_words=n_top_words)
     # get_LDA_topics(documents, no_topics=25, no_top_words=20, display=1)
     results = calculate_cosine_similarity(documents, folder_path, query_path)
     
-    print(articles_in_descending_relevance(folder_path, results))
+    print(get_articles_with_descending_relevance(folder_path, results))
 
 
 if __name__ == "__main__":
